e3c6c03cca59c50e103d8a696d41ceb1151ff6ac
Shibin George <george.shibin1993@gmail.com>
2019-11-30 16:57:40 -0500

    Output cluster metadata into another cluster.out file

--- Indexer/src/apps/ClusteringApp.java
+++ Indexer/src/apps/ClusteringApp.java
@@ -44,68 +44,81 @@ public class ClusteringApp {
         // similarity method to be used
         Similarity similarity = new CosineSimilarity(index);
 
-        // iterate from threshold values from 0.05 to 0.95 increments of 0.05
-        for (int t = 5; t <= 95; t += 5) {
+        try {
+            PrintWriter clusterMetadataWriter = new PrintWriter("cluster.out");
+            // iterate from threshold values from 0.05 to 0.95 increments of 0.05
+            for (int t = 5; t <= 95; t += 5) {
 
-            double threshold = (double) t / 100.0;
+                double threshold = (double) t / 100.0;
 
-            // new set of clusters for this threshold
-            ArrayList<Cluster> clusters = new ArrayList<Cluster>();
+                // new set of clusters for this threshold
+                ArrayList<Cluster> clusters = new ArrayList<Cluster>();
 
-            // cluster sequence number
-            int cId = 1;
+                // cluster sequence number
+                int cId = 1;
 
-            ArrayList<String> docIDs = index.getBackingDocumentIDs();
+                ArrayList<String> docIDs = index.getBackingDocumentIDs();
 
-            System.out.println("threshold: " + threshold);
+                System.out.println("threshold: " + threshold);
 
-            for (int doc = 0; doc < numDocs; doc++) {
+                for (int doc = 0; doc < numDocs; doc++) {
 
-                DocumentVector documentVector = documentVectorFactory.getDocumentVector(doc);
+                    DocumentVector documentVector = documentVectorFactory.getDocumentVector(doc);
 
-                double bestScore = Double.MIN_VALUE;
-                Cluster bestCluster = null;
+                    double bestScore = Double.MIN_VALUE;
+                    Cluster bestCluster = null;
 
-                // compute distance of this doc with each cluster
-                for (Cluster cluster : clusters) {
-                    double score = cluster.score(documentVector);
+                    // compute distance of this doc with each cluster
+                    for (Cluster cluster : clusters) {
+                        double score = cluster.score(documentVector);
 
-                    if (score > bestScore) {
-                        bestCluster = cluster;
-                        bestScore = score;
+                        if (score > bestScore) {
+                            bestCluster = cluster;
+                            bestScore = score;
+                        }
+                    }
+
+                    if (bestCluster != null && bestScore > threshold) {
+                        bestCluster.addDocument(documentVector);
+                    } else {
+
+                        // a new cluster
+                        Cluster cluster = new Cluster(cId++, Linkage.MEAN, similarity);
+
+                        // add this document to the new cluster
+                        cluster.addDocument(documentVector);
+
+                        // add the cluster into the list of clusters
+                        clusters.add(cluster);
                     }
                 }
 
-                if (bestCluster != null && bestScore > threshold) {
-                    bestCluster.addDocument(documentVector);
-                } else {
-
-                    // a new cluster
-                    Cluster cluster = new Cluster(cId++, Linkage.MEAN, similarity);
-
-                    // add this document to the new cluster
-                    cluster.addDocument(documentVector);
-
-                    // add the cluster into the list of clusters
-                    clusters.add(cluster);
-                }
-            }
-
-            // dump the cluster info into a file
-            try {
-                PrintWriter printWriter = new PrintWriter("cluster-" + threshold + ".out");
-                clusters.stream().forEach((cluster) -> {
-                    for (DocumentVector document : cluster.getDocumentsInCluster()) {
-                        printWriter.write(cluster.getClusterId()
+                // dump the cluster info into a file
+                try {
+                    PrintWriter printWriter = new PrintWriter("cluster-" + threshold + ".out");
+                    clusters.stream().forEach((cluster) -> {
+                        clusterMetadataWriter.write(threshold
+                                + prettyPrintSpaces(10, threshold + "")
+                                + cluster.getClusterId()
                                 + prettyPrintSpaces(10, cluster.getClusterId().toString())
-                                + docIDs.get(document.getDocId()).split("#")[1] + "\n");
-                    }
-                });
-                printWriter.close();
-            } catch (FileNotFoundException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+                                + cluster.getDocumentsInCluster().size() + "\n");
+                        for (DocumentVector document : cluster.getDocumentsInCluster()) {
+                            printWriter.write(cluster.getClusterId()
+                                    + prettyPrintSpaces(10, cluster.getClusterId().toString())
+                                    + docIDs.get(document.getDocId()).split("#")[1] + "\n");
+                        }
+                    });
+                    printWriter.close();
+                } catch (FileNotFoundException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
             }
+
+            clusterMetadataWriter.close();
+        } catch (FileNotFoundException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
         }
     }
 }
